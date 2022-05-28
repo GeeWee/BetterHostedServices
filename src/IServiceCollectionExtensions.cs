c98492956671c42d8235cd5f19d62cc355666bc1
ProMix0 <misha.zavertkin@mail.ru>
2022-05-28 12:02:51 +0700

    closes #28

--- src/IServiceCollectionExtensions.cs
+++ src/IServiceCollectionExtensions.cs
@@ -14,11 +14,8 @@ namespace BetterHostedServices
         /// Add the scaffolding needed to make the BetterHostedServices etc. work.
         /// Should be called once before making any IHostedServices run.
         /// </summary>
-        public static IServiceCollection AddBetterHostedServices(this IServiceCollection serviceCollection)
-        {
-            return serviceCollection
+        public static IServiceCollection AddBetterHostedServices(this IServiceCollection serviceCollection) => serviceCollection
                 .AddTransient<IApplicationEnder, ApplicationLifetimeEnder>();
-        }
 
         /// <summary>
         /// This registers an IHostedService, both as a HostedService, but also as a Singleton in the DI container
@@ -28,13 +25,12 @@ namespace BetterHostedServices
         /// </summary>
         /// <typeparam name="TService">The interface type that the IHostedService should be available under in the DI</typeparam>
         /// <typeparam name="TImplementation">The concrete type of the IHostedService to instantiate</typeparam>
-        public static void AddHostedServiceAsSingleton<TService, TImplementation>(this IServiceCollection services)
+        public static IServiceCollection AddHostedServiceAsSingleton<TService, TImplementation>(this IServiceCollection services)
             where TService : class
             where TImplementation : class, IHostedService, TService
-        {
-            services.AddSingleton<TService, TImplementation>();
-            services.AddHostedService<HostedServiceWrapper<TService>>();
-        }
+            => services.AddSingleton<TService, TImplementation>()
+                       .AddHostedService<HostedServiceWrapper<TService>>();
+
 
 
         /// <summary>
@@ -44,12 +40,10 @@ namespace BetterHostedServices
         /// Heavily inspired by https://stackoverflow.com/a/52398431/8007580
         /// </summary>
         /// <typeparam name="TService">The concrete type of the IHostedService to instantiate.</typeparam>
-        public static void AddHostedServiceAsSingleton<TService>(this IServiceCollection services)
+        public static IServiceCollection AddHostedServiceAsSingleton<TService>(this IServiceCollection services)
             where TService : class, IHostedService
-        {
-            services.AddSingleton<TService>();
-            services.AddHostedService<HostedServiceWrapper<TService>>();
-        }
+            => services.AddSingleton<TService>()
+                       .AddHostedService<HostedServiceWrapper<TService>>();
 
         /// <summary>
         /// Add a periodic task.
@@ -59,27 +53,17 @@ namespace BetterHostedServices
         /// <param name="failureMode">Determines how the service behaves when a task fails.</param>
         /// <param name="timeBetweenTasks">How long after the completion of the previous task, should the next task run?</param>
         /// <typeparam name="TPeriodicTask">The periodic task to run</typeparam>
-        public static void AddPeriodicTask<TPeriodicTask>(this IServiceCollection services, PeriodicTaskFailureMode failureMode, TimeSpan timeBetweenTasks)
+        public static IServiceCollection AddPeriodicTask<TPeriodicTask>(this IServiceCollection services, PeriodicTaskFailureMode failureMode, TimeSpan timeBetweenTasks)
             where TPeriodicTask : class, IPeriodicTask
-        {
-            services.AddTransient<TPeriodicTask>();
+            => services.AddTransient<TPeriodicTask>()
 
-            services.AddHostedService<PeriodicTaskRunnerBackgroundService<TPeriodicTask>>((services) =>
-            {
-                return new PeriodicTaskRunnerBackgroundService<TPeriodicTask>(
-                    applicationEnder: services.GetRequiredService<IApplicationEnder>(),
-                    logger: services.GetRequiredService<ILogger<PeriodicTaskRunnerBackgroundService<TPeriodicTask>>>(),
-                    serviceProvider: services.GetRequiredService<IServiceProvider>(),
-                    periodicTaskFailureMode: failureMode,
-                    timeBetweenTasks: timeBetweenTasks
-                );
-            });
-        }
-
-
-
+                    .AddHostedService((services) =>
+                        new PeriodicTaskRunnerBackgroundService<TPeriodicTask>(
+                            applicationEnder: services.GetRequiredService<IApplicationEnder>(),
+                            logger: services.GetRequiredService<ILogger<PeriodicTaskRunnerBackgroundService<TPeriodicTask>>>(),
+                            serviceProvider: services.GetRequiredService<IServiceProvider>(),
+                            periodicTaskFailureMode: failureMode,
+                            timeBetweenTasks: timeBetweenTasks
+                    ));
     }
-
-
-
 }
