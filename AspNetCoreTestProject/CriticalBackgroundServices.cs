89a26f3fbe09f52376501a90fce6096ba1c5c0f2
ProMix0 <misha.zavertkin@mail.ru>
2022-05-30 18:05:56 +0700

    (ASP) Inherit abstract CriticalBackgroundservice.OnExit() in tests

--- AspNetCoreTestProject/CriticalBackgroundServices.cs
+++ AspNetCoreTestProject/CriticalBackgroundServices.cs
@@ -10,6 +10,11 @@ namespace AspNetCoreTestProject
         protected override Task ExecuteAsync(CancellationToken stoppingToken) =>
             throw new Exception("Crash right away");
 
+        protected override void OnError(Exception exceptionFromExecuteAsync)
+        {
+            this._applicationEnder.ShutDownApplication();
+        }
+
         public ImmediatelyCrashingCriticalBackgroundService(IApplicationEnder applicationEnder) : base(applicationEnder)
         {
         }
@@ -21,6 +26,11 @@ namespace AspNetCoreTestProject
         {
             await Task.Yield(); // Hand over control explicitly, to ensure this behaviour also works
             throw new Exception("Crash after yielding");
+        }
+
+        protected override void OnError(Exception exceptionFromExecuteAsync)
+        {
+            this._applicationEnder.ShutDownApplication();
         }
 
         public YieldingAndThenCrashingCriticalBackgroundService(IApplicationEnder applicationEnder) : base(applicationEnder)
@@ -36,6 +46,11 @@ namespace AspNetCoreTestProject
         {
             await Task.Yield(); // Hand over control explicitly
             this.Activated = true;
+        }
+
+        protected override void OnError(Exception exceptionFromExecuteAsync)
+        {
+            this._applicationEnder.ShutDownApplication();
         }
 
         public StubCriticalBackgroundService(IApplicationEnder applicationEnder) : base(applicationEnder)
